66860f698a7fec069e967e480fb00ac387299cd4
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-01-19 12:52:43 +0530

    Time: 1 ms (87.97%), Space: 44.2 MB (45.25%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,21 @@
 class Solution {
-    private static List<List<Integer>> ans = new ArrayList<>();
+    static List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
         ans.clear();
-        helper(nums,0,new ArrayList<>());
-        System.out.println(ans.size());
-        return ans;
+        helper(nums,0,new ArrayList<Integer>());
+        return ans;  
     }
-    public static void helper(int[] nums,int idx,ArrayList<Integer>li){
+
+    public static void helper(int nums[],int idx,ArrayList<Integer>li){
 
         if (idx == nums.length){
             ans.add(new ArrayList<>(li));
             return;
         }
-
+        
         li.add(nums[idx]);
         helper(nums,idx+1,li);
         li.remove(li.size()-1);
         helper(nums,idx+1,li);
-
     }
 }
