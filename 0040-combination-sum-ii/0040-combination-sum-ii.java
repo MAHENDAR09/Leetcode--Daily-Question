b6b84403515a2f72365edc2ad02ff7abee80a308
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-01-19 14:47:49 +0530

    Time: 193 ms (16.31%), Space: 53.2 MB (5.06%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,32 +1,33 @@
 class Solution {
-    private static List<List<Integer>> ans = new ArrayList<>();
+    static List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         ans.clear();
         Arrays.sort(candidates);
-        helper(candidates,target,0,0,new ArrayList<>());
+        helper(candidates,0,0,target,new ArrayList<>());
         return ans;
     }
-
-    public static void helper(int[] nums,int target,int sum,int idx,List<Integer>li){
+    public static void helper(int[] nums,int idx,int sum,int target,ArrayList<Integer>li){
 
         if (sum == target){
             ans.add(new ArrayList<>(li));
             return;
         }
-        if (nums.length == idx || target < sum)
+
+        if (idx == nums.length || sum > target){
+            System.out.println(li);
             return;
+        }
 
         for (int i=idx;i<nums.length;i++){
 
-            if (i > idx && nums[i] == nums[i-1])
+            if (i > idx && nums[i-1] == nums[i])
                 continue;
 
-            sum = sum + nums[i];
             li.add(nums[i]);
-            helper(nums,target,sum,i+1,li);
-            sum = sum - nums[i];
+            helper(nums,i+1,sum+nums[i],target,li);
             li.remove(li.size()-1);
         }
+
 
     }
 }
