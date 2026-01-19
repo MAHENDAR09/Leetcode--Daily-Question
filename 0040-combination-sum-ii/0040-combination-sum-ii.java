3fddd68cf955251ce6c358cce7e347526f195e6e
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-01-19 14:49:20 +0530

    Time: 6 ms (71.17%), Space: 45.3 MB (57.6%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -3,18 +3,17 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         ans.clear();
         Arrays.sort(candidates);
-        helper(candidates,0,0,target,new ArrayList<>());
+        helper(candidates,0,target,new ArrayList<>());
         return ans;
     }
-    public static void helper(int[] nums,int idx,int sum,int target,ArrayList<Integer>li){
+    public static void helper(int[] nums,int idx,int target,ArrayList<Integer>li){
 
-        if (sum == target){
+        if (target == 0){
             ans.add(new ArrayList<>(li));
             return;
         }
 
-        if (idx == nums.length || sum > target){
-            System.out.println(li);
+        if (idx == nums.length || target < 0){
             return;
         }
 
@@ -24,7 +23,7 @@ class Solution {
                 continue;
 
             li.add(nums[i]);
-            helper(nums,i+1,sum+nums[i],target,li);
+            helper(nums,i+1,target-nums[i],li);
             li.remove(li.size()-1);
         }
 
