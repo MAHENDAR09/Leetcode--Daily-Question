e7ce65c88820e6fbba8e82744f0c9ab9b0fc8a29
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-01-19 15:04:02 +0530

    Time: 13 ms (98.91%), Space: 98 MB (54.39%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,33 @@
+class Solution {
+    /*
+        * Solved using 1 based index
+        * after going to n+1 index i am checking so 
+        * base condition is at bottom.
+    */
+    static List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        
+        ans.clear();
+        helper(n,k,1,new ArrayList<>());
+        return ans;
+    }
+    public static void helper(int n,int k,int idx,ArrayList<Integer>li){
+
+        if (li.size() == k){
+            ans.add(new ArrayList<>(li));
+            return ;
+        }
+
+        if(idx == (n+1)){
+            return;
+        }
+
+        for (int i=idx;i<=n;i++){
+
+            li.add(i);
+            helper(n,k,i+1,li);
+            li.remove(li.size()-1);
+
+        }
+    }
+}
