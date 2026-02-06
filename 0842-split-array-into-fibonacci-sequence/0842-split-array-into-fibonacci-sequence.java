dfd273be2180409ec27fc7450f9087a414b9241a
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-02-06 14:38:55 +0530

    Time: 10 ms (12.88%), Space: 52.1 MB (6.82%) - LeetHub

--- 0842-split-array-into-fibonacci-sequence/0842-split-array-into-fibonacci-sequence.java
+++ 0842-split-array-into-fibonacci-sequence/0842-split-array-into-fibonacci-sequence.java
@@ -4,25 +4,17 @@ class Solution {
     public List<Integer> splitIntoFibonacci(String num) {
         ans.clear();
         list.clear();
-        helper(num,0,"");
+        helper(num,0);
         return list;
     }
-    public static void helper(String num,int idx,String x){
-
-        // if (idx  == num.length()){
-        //     System.out.println(ans);
-        //     return;
-        // }
-        
-
+    public static void helper(String num,int idx){
         if (ans.size() >= 3){
             int n = ans.size();
             if (! ((long) ans.get(n-1) == (ans.get(n-2)* 1L) + ans.get(n-3))){
                 return;
             }
             
-            if (idx == num.length() && x.length() == num.length()){
-                    System.out.println(ans);
+            if (idx == num.length() && getLength(ans) == num.length()){
                     list.addAll(ans);
                     return ;
             }
@@ -36,13 +28,16 @@ class Solution {
             if (f > Integer.MAX_VALUE)
                 return;
             ans.add( (int) f);
-            x += Long.toString(f);
-            helper(num,i+1,x);
+            helper(num,i+1);
             String temp = Integer.toString(ans.remove(ans.size()-1));
-            x = x.substring(0,x.length()-temp.length());
         }
+    }
+    public static int getLength(List<Integer> s){
+        int len = 0;
+        for (int x : s){
+            len += String.valueOf(x).length();
+        }
+        return len;
     }
 }
 
-
-// 1 -> 10 ->
