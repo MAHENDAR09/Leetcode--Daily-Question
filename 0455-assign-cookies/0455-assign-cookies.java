81f936e6496c8b4e85c93a1d66ecb3b81de3774b
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-02-23 11:43:55 +0530

    Time: 15 ms (62.07%), Space: 51.8 MB (5.65%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,18 +1,24 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        
         Arrays.sort(g);
         Arrays.sort(s);
-        int cnt=0;
-        for (int i=0;i<g.length;i++) {
-            for (int j=0;j<s.length;j++) {
-                if (s[j] >= g[i]) {
-                    System.out.println (i+" "+j);
-                    cnt++;
-                    s[j] = 0;
-                    g[i] = Integer.MAX_VALUE;
-                }
+
+        int sIdx = 0;
+        int gIdx = 0;
+        int cookies = 0;
+        
+        while (sIdx < s.length && gIdx < g.length){
+            if(g[gIdx] <= s[sIdx]){
+                cookies++;
+                sIdx++;
+                gIdx++;
             }
+            else if (g[gIdx] > s[sIdx]){
+                sIdx++;
+            }
+            
         }
-        return cnt;
+        return cookies;
     }
 }
