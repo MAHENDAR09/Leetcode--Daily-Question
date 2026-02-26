a7c280f97c024699f7d69e60a80dcf2a7b9150cf
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-02-26 22:53:58 +0530

    Time: 2 ms (40.12%), Space: 42.6 MB (88.5%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -9,37 +9,26 @@ class Solution {
 
             if (sb.charAt(last) == '1' ){
                 sb = addOne(sb);
-                cnt++;
             }
             else {
                 sb = sb.deleteCharAt(last);
-                cnt++;
             }
+            cnt++;
         }
         return cnt;
     }
     public static StringBuilder addOne(StringBuilder s){
 
-        int n = s.length() - 1;
-        int carry = 1;
-        
-
-        for (int i=n;i>=0;i--){
-            int sum = carry + (int) s.charAt(i) - '0';
-           if (sum == 2) {
-            s.setCharAt(i,'0');
-           }
-           else if (sum == 1){
-            s.setCharAt(i,'1');
-            carry = 0;
-           }
+        for (int i=s.length()-1;i>=0;i--){
+            
+            if (s.charAt(i) == '0'){
+                s.setCharAt(i,'1');
+                return s;
+            }
+            else 
+                s.setCharAt(i,'0');
         }
-
-        if (carry == 1)
-            s.insert(0, '1');
+        s.insert(0, '1');
         return s;
     }
 }
-
-// 1110
-// 100
