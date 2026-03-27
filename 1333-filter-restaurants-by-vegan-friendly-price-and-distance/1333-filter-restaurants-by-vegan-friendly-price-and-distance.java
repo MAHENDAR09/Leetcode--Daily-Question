0dbddbad5e4e35ac037f9c502c37542e110e0f70
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-03-27 17:07:56 +0530

    Time: 12 ms (19.59%), Space: 60.8 MB (10.31%) - LeetHub

--- 1333-filter-restaurants-by-vegan-friendly-price-and-distance/1333-filter-restaurants-by-vegan-friendly-price-and-distance.java
+++ 1333-filter-restaurants-by-vegan-friendly-price-and-distance/1333-filter-restaurants-by-vegan-friendly-price-and-distance.java
@@ -1,63 +1,18 @@
-class Solution {
+ class Solution {
     public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
 
-        List<Integer> ans = new ArrayList<Integer>();
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>( (a,b) ->  {
-
-            int res = Integer.compare(b.rating,a.rating);
-
+        return 
+    Arrays.stream(restaurants)
+    .filter(e -> e[3] <= maxPrice && e[4] <= maxDistance && e[2] >= veganFriendly)
+    .sorted( (a,b) -> {
+        int res = Integer.compare(b[1],a[1]);
             if (res != 0)
                 return res;
-            return Integer.compare(b.id,a.id);
-        }  
-        );
-
-        for (int ar[] : restaurants){
-                if (accept(ar,maxPrice,maxDistance,veganFriendly))
-                    pq.add(processData(ar));
-        }
-
-        while (!pq.isEmpty()){
-            ans.add(pq.poll().getIdx());
-        }
-
-        return ans;
+            return Integer.compare(b[0],a[0]);
+    })
+    .mapToInt(e -> e[0])
+    .boxed()
+    .toList()
+    ;
     }
-
-    public static boolean accept(int d[],int maxP,int maxD,int veganFriendly){
-
-        if (d[3] > maxP || d[4] > maxD)
-            return false;
-
-        return d[2] >= veganFriendly;
-    }
-    public static Node processData(int d[]){
-        return new Node(d[0],d[1],d[2],d[3],d[4]);
-    }
-}
-class Node {
-
-    public int id;
-    public int rating;
-    public int veganFriendly;
-    public int maxPrice;
-    public int maxDistance;
-
-    Node (int id,int rating,int veganFriendly,int maxPrice,int maxDistance){
-        this.id = id;
-        this.rating = rating;
-        this.veganFriendly = veganFriendly;
-        this.maxPrice = maxPrice;
-        this.maxDistance = maxDistance;
-    }
-
-    public int getIdx(){
-        return id;
-    }
-    public String toString(){
-        // return veganFriendly+" "+maxPrice+" "+maxDistance;
-        return id +" ";
-    }
-        
 }
