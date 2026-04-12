023812a400318fc68570d28bc03b89e12b9071bc
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-04-12 10:17:34 +0530

    Time: 1 ms (100%), Space: 43.2 MB (100%) - LeetHub

---
+++ 3894-traffic-signal-color/3894-traffic-signal-color.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String trafficSignal(int timer) {
+
+        return 
+        timer == 0 ? "Green" : timer == 30  ? "Orange" :
+        
+         timer > 30 && timer <= 90 ? "Red" : "Invalid"; 
+        
+    }
+}
