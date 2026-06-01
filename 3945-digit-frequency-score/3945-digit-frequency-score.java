0c41cdd7f08ddfaf5686de82d80a9db6e8f9de39
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-06-01 12:52:03 +0530

    Time: 1 ms (100%), Space: 42.4 MB (100%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,19 +1,15 @@
 class Solution {
     public int digitFrequencyScore(int n) {
 
-        int f[] = new int[10];
         int score = 0;
 
         while (n != 0){
 
             int rem = n % 10;
-            f[rem]++;
+            score += rem;
             n /= 10;
         }
 
-        for (int i=0;i<10;i++){
-            score += (i * f[i]);
-        }
         return score;
     }
 }
