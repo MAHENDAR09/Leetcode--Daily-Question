775d94133da1ecc585f4bc8b7e830bb1f1515c1a
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-09-17 00:03:39 +0530

    Time: 282 ms (5.03%), Space: 246.3 MB (5.39%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,37 +1,20 @@
 class MyHashMap {
 
-    Node map[];
+    int map[];
     public MyHashMap() {
-        map = new Node[10_000_001];
+        map = new int[10_000_001];
+        Arrays.fill(map,-1);
     }
     
     public void put(int key, int value) {
-        map[key] = new Node(key,value);
+        map[key] = value;
     }
     
     public int get(int key) {
-
-        if (map[key] == null)
-            return -1;
-        return map[key].getValue();
+        return map[key] == -10 ? -1 : map[key];
     }
     
     public void remove(int key) {
-        if (map[key] == null)
-            return ;
-        map[key] = null;
-    }
-}
-class Node {
-
-    private int key,value;
-
-    public Node (int key,int value){
-        this.key = key;
-        this.value = value;
-    }
-
-    public int getValue(){
-        return value;
+        map[key] = -10;
     }
 }
