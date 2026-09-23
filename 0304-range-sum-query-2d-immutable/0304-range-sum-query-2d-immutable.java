902ce93705efc60c1cd146839e2dee7e8565d2e2
Mahendar <126066500+MAHENDAR09@users.noreply.github.com>
2026-09-23 16:55:53 +0530

    Time: 121 ms (13.35%), Space: 140.7 MB (91.29%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,19 +1,51 @@
 class NumMatrix {
 
     int matrix[][];
+    int ps[][];
+    boolean isCal = true;
+    int m ;
 
     public NumMatrix(int[][] matrix) {
         this.matrix= matrix;
+        this.ps = matrix;
+        this.m = matrix[0].length;
     }
     
+    private void calPS(){
+
+        for (int i=0;i<matrix.length;i++){
+            for (int j=1;j<matrix[0].length;j++){
+                ps[i][j] = ps[i][j-1] + matrix[i][j];
+            }
+        }
+
+        // for (int e[] : ps){
+
+        //     for (int i=0;i<e.length;i++)
+        //         System.out.print(e[i]+" ");
+
+        //     System.out.println();
+        // }
+    }
     public int sumRegion(int row1, int col1, int row2, int col2) {
+ 
+        if (isCal){
+            calPS();
+            isCal = false;
+        }
+
 
         int sum = 0;
 
-        for (int i=row1;i<=row2;i++){
-            for (int j=col1;j<=col2;j++){
-                sum += matrix[i][j];
-            }
+        while (row1 <= row2){
+
+            int s1 = (col1 - 1) == -1  ? 0  : ps[row1][col1-1];
+            int s2 =  ps[row1][col2];
+
+            // (col2 + 1) ==  m ? ps[row1][col2] (evertime it needs the same)
+
+            sum = sum + (s2 - s1);
+            row1++;
         }
 
         return sum;
